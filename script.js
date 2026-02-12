c7a7fab43dbf4877941bfd0aa0892cf3292ed0d7
P2marketing <comprasp2mkt@gmail.com>
2026-02-11 21:04:56 -0300

    up 2

--- script.js
+++ script.js
@@ -23,7 +23,8 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         e.preventDefault();
         const target = document.querySelector(this.getAttribute('href'));
         if (target) {
-            const headerOffset = 70;
+            const header = document.querySelector('.header');
+            const headerOffset = header ? header.offsetHeight : 70;
             const elementPosition = target.getBoundingClientRect().top;
             const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
 
